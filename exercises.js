a27426245120d9abe75cd36a953683d52f502cbd
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-05-04 14:43:16 -0400

    solved the second exercise

--- exercises.js
+++ exercises.js
@@ -1,4 +1,4 @@
-(function() {
+    (function() {
   'use strict';
 
   // ---------------------
@@ -7,19 +7,26 @@
  function max(num1, num2){
    if (num1 > num2) {
      return num1;
-   } else if (num2 > num1) {
+   } else {
      return num2;
    }
 
    }
-   var test = max(5, 10);
-
 
 
   // ---------------------
   // Define a function maxOfThree() that takes three numbers as arguments and returns the largest of them.
   // ---------------------
+  function maxOfThree(num1, num2, num3){
+    if (num1 > num2 && num1 > num3) {
+      return num1;
+    } else if (num2 > num1 && num2 > num3 ) {
+      return num2;
+    } else {
+      return num3;
+    }
 
+  }
 
 
   // ---------------------
