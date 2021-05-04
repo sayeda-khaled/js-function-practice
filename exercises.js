63f8c9258af986bf46d55391625d885d0b53af6e
Sayeda Khaled <sayedakhaled@Sayedas-MacBook-Pro.local>
2021-05-04 15:13:43 -0400

    Third exercise

--- exercises.js
+++ exercises.js
@@ -33,6 +33,13 @@
   // Define a function isVowel() that takes a character (i.e. a string of length 1) and returns true if it is a vowel, false otherwise.
   // ---------------------
 
+  function isVowel(x) {
+    if (x == 'a' || x == 'e' || x == 'i' || x == 'o' || x == 'u') {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
 
   // ---------------------
